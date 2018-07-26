887400440e505e594b530603e12b727368f570e0
xiezg <xiezg@500wan.com>
2018-07-26 13:57:34 +0800

    '726'

--- js/metamask.js
+++ js/metamask.js
@@ -1,28 +1,67 @@
+var web3_accountAddr = 0
+var web3_gasPrice = 0
+
 if (typeof web3 !== "undefined") {
     console.log(web3)
     console.log(web3.version)
     web3.eth.getBlock(48, function (error, result) {
-        if (!error){
-            console.log(JSON.stringify(result));
+        if (!error) {
             console.log((result));
         } else {
             console.error(error);
         }
     })
+    /* 获取当前账号地址 */
+    web3.eth.getAccounts(function (err, res) {
+        if (!err) {
+            console.log(res.toString());  // []
+            if (res.length > 0) {
+                web3_accountAddr = res.toString()
+                console.log(web3_accountAddr);
+                console.log('===当前地址======');
+            }
+        } else {
+            console.error('getAccount' + error);
+        }
+    })
+    /* 获取当前gasPrice */
+    web3.eth.getGasPrice(function (err, res) {
+        if (!err) {
+            if (res) {
+                web3_gasPrice = res.toNumber(10)
+                console.log(web3_gasPrice);
+                console.log('===当前gas======');
+            }
+        } else {
+            console.error('getAccount' + error);
+        }
+    })
+    /* 获取合约相关数据 */
+
+
 } else {
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 
-$('#js_tranMask').on('click',function () {
+/*
+*   transfer methods
+*
+* */
+function transKey(fromAddr, toAddr, keyVal, gas, sendData) {
+    fromAddr = fromAddr || '0x81Fa761dB3bFA16aE9Bb7c41ec7993def65cf386';
+    toAddr = toAddr || '0x2b5006d3dce09dafec33bfd08ebec9327f1612d8';
+    keyVal = keyVal || '1';
+    gas = gas || '31000';
+    sendData = sendData || '123';
     if (typeof web3 !== 'undefined') {
         web3.eth.sendTransaction({
-            from: '0x81Fa761dB3bFA16aE9Bb7c41ec7993def65cf386',
+            from: fromAddr,
             to: Msg.withdraw_addr,
             value: web3.toWei(Number(Msg.amount), 'ether'),   // The value transferred for the transaction in wei
-            gas: 31000,
-            data: web3.toHex(Msg.uid + '$' + Msg.oid)
-        }, function (err, hash) {
-            console.log(hash);
+            gas: parseFloat(gas),
+            data: sendData
+        }, function (err, res) {
+            console.log(res);
         })
     } else {
         this.$message({
@@ -31,4 +70,6 @@ $('#js_tranMask').on('click',function () {
             duration: 1200
         })
     }
-})
+}
+
+$('#js_tranMask').on('click',)
