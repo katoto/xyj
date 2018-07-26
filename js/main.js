6e41ec613823b82b12540d370e03f4827a4c6cf2
Richole <richole.yu@qq.com>
2018-07-26 19:58:47 +0800

    更新播报

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-$(function () {
+window.onload = function () {
+    // 渲染时间
     function renderTime (hour, min, second) {
         function formatTime (time) {
             return time < 10 ? '0' + time.toString() : time;
@@ -6,6 +7,7 @@ $(function () {
         $('.headtimer').text(formatTime(hour) + ':' + formatTime(min) + ':' + formatTime(second));
     }
 
+    // 根据time计算小时 分钟 秒数
     function calcTime (time) {
         var hour = Math.floor(time / 3600);
         var min = Math.floor((time - (hour * 3600)) / 60);
@@ -13,7 +15,8 @@ $(function () {
         return [hour, min, second]
     }
     
-    function createInterval (time) {
+    // 更新计数器
+    function updateInterval (time) {
         if (xyj._timer) {
             clearInterval(xyj._timer);
         }
@@ -25,13 +28,30 @@ $(function () {
         }, 1000);
     }
 
+    // 播报弹窗
+    function showMsg (msg) {
+        if (xyj._msgTimer) {
+            // 当前存在播报
+        } else {
+            // 当前没有播报
+            $('.pop-success').text(msg)
+            $('.pop-success').addClass('show')
+            xyj._msgTimer = setTimeout(function () {
+                $('.pop-success').removeClass('show')
+            }, 6000)
+        }
+    }
+    
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+
+    // 获取合约剩余时间
     xyj.getTimeLeft(function (error, time) {
         if (error) {
             console.log(error)
         } else {
-            createInterval(time)
+            updateInterval(time)
         }
     });
-})()
-
-// 合约剩余时间
+}
