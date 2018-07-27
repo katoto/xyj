aea8ebb1f6e91576cb9d8338c7b72a8fdace8aef
guowf <380482989@qq.com>
2018-07-27 16:50:40 +0800

    '0727'

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,25 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'), //服务器
     rename = require('gulp-rename'),
     autoprefixer = require('autoprefixer'),
-    less = require('gulp-less')
+    less = require('gulp-less'),
+    postcss = require('gulp-postcss'),
+    px2rem = require('postcss-px2rem'),
+    notify = require("gulp-notify");
 
 gulp.task('less', function () {
+    var processors = [px2rem({
+        remUnit: 53.333
+    }), autoprefixer({
+        brpwsers: ['last 2 version', 'android >= 4.0']
+    })];
     return gulp.src('./css/*.less')
         .pipe(less())
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('./css'));
+        .pipe(postcss(processors))
+        .pipe(notify("Hello Gulp!"))
+        .pipe(gulp.dest('./css'))
 });
 
 //服务器
